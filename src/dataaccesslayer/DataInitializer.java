10ebcfc48e27fc056ae9c159076a371ba3cfa710
Kemal Turk <okulmavi@gmail.com>
2021-05-24 17:38:12 +0300

    minor change

--- src/dataaccesslayer/DataInitializer.java
+++ src/dataaccesslayer/DataInitializer.java
@@ -5,10 +5,10 @@ import businesslayer.model.*;
 
 public class DataInitializer implements IDataInitializer {
 
-    private PetCreator dogCreator = new DogCreator();
-    private PetCreator catCreator = new CatCreator();
-    private UserCreator adminCreator = new AdminCreator();
-    private UserCreator ownerCreator = new OwnerCreator();
+    private final PetCreator dogCreator = new DogCreator();
+    private final PetCreator catCreator = new CatCreator();
+    private final UserCreator adminCreator = new AdminCreator();
+    private final UserCreator ownerCreator = new OwnerCreator();
 
     @Override
     public HotelAdmin getInitialAdmin() {
@@ -19,25 +19,25 @@ public class DataInitializer implements IDataInitializer {
         owner1.addNewPet(catCreator.createPet("Bella", 3, 8));
         owner1.addNewPet(dogCreator.createPet("Cooper", 6, 12));
 
-        Owner owner2 = (Owner) ownerCreator.createUser("dilekozturk", "dilek123");
-        owner2.addNewPet(catCreator.createPet("Kitty", 1, 3));
+        Owner owner2 = (Owner) ownerCreator.createUser("serapsahin", "serap123");
+        owner2.addNewPet(catCreator.createPet("Pepper", 3, 15));
+        owner2.addNewPet(catCreator.createPet("Millie", 3, 15));
 
-        Owner owner3 = (Owner) ownerCreator.createUser("serhatcaner", "serhat123");
-        owner3.addNewPet(dogCreator.createPet("Lola", 3, 7));
-        owner3.addNewPet(dogCreator.createPet("Moly", 4, 7));
-        owner3.addNewPet(dogCreator.createPet("Duke", 3, 7));
+        Owner owner3 = (Owner) ownerCreator.createUser("dilekozturk", "dilek123");
+        owner3.addNewPet(catCreator.createPet("Kitty", 1, 3));
 
-        Owner owner4 = (Owner) ownerCreator.createUser("canertanguler", "caner123");
-        owner4.addNewPet(catCreator.createPet("Lucy", 5, 5));
-        owner4.addNewPet(dogCreator.createPet("Zeus", 4, 12));
+        Owner owner4 = (Owner) ownerCreator.createUser("serhatcaner", "serhat123");
+        owner4.addNewPet(dogCreator.createPet("Lola", 3, 7));
+        owner4.addNewPet(dogCreator.createPet("Moly", 4, 7));
+        owner4.addNewPet(dogCreator.createPet("Duke", 3, 7));
 
-        Owner owner5 = (Owner) ownerCreator.createUser("kemalturk", "kemal123");
-        owner5.addNewPet(catCreator.createPet("Zagreus", 2, 10));
-        owner5.addNewPet(dogCreator.createPet("Cerberus", 6, 3));
+        Owner owner5 = (Owner) ownerCreator.createUser("canertanguler", "caner123");
+        owner5.addNewPet(catCreator.createPet("Lucy", 5, 5));
+        owner5.addNewPet(dogCreator.createPet("Zeus", 4, 12));
 
-        Owner owner6 = (Owner) ownerCreator.createUser("serapsahin", "serap123");
-        owner6.addNewPet(catCreator.createPet("Pepper", 3, 15));
-        owner6.addNewPet(catCreator.createPet("Millie", 3, 15));
+        Owner owner6 = (Owner) ownerCreator.createUser("kemalturk", "kemal123");
+        owner6.addNewPet(catCreator.createPet("Zagreus", 2, 10));
+        owner6.addNewPet(dogCreator.createPet("Cerberus", 6, 3));
 
 
         hotelAdmin.addOwner(owner1); hotelAdmin.addOwner(owner2); hotelAdmin.addOwner(owner3);
